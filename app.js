8e0ef61cdfa1cba2eec9690bb2549ab03c9c4762
nikola_cobra <nikola@braukmueller.de>
2018-10-16 14:21:21 +0200

    changes in app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var app = express();
 app.use('/', express.static(`${__dirname}/public`));
 
 // Binding express app to port 3000
-app.listen(8080, function(){
-    console.log((new Date()) + " Server is listening on port 8080");
+app.listen(3000, function(){
+    console.log((new Date()) + " Server is listening on port 3000");
 });
 
 //Verbindung zur Datenbank
