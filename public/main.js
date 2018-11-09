a6191537cb0454edb1bc15718c52699e8dd72285
nikola_cobra <nikola@braukmueller.de>
2018-11-09 14:33:45 +0100

    correction, points get displayed

--- public/main.js
+++ public/main.js
@@ -1,9 +1,10 @@
-
-
 //Global variables
 
 var dLat;       //to save Latitude
 var dLng;       //to save Longitude
+
+
+
 
 function onMapClick(e) {
     popup
@@ -16,32 +17,36 @@ function onMapClick(e) {
     dLat = e.latlng.lat;
     dLng = e.latlng.lng;
 
-  }
+}
+
 
 function saveRoute() {
     $.ajax({
         type: 'POST',
-        data: {route: {name: 'test'}},
+        data: { route: { name: 'test' } },
         datatype: 'json',
         url: '/saveRoute',
     });
 
-} 
+}
 
 function newPointOfInterest() {
     let oPoint = {};
     oPoint.name = document.getElementById("sName").value;
     oPoint.description = document.getElementById("sDescription").value;
-    oPoint.category = document.getElementById("iCategory").value;
-    oPoint.latitude= dLat;
-    oPoint.longitude = dLng;
+    oPoint.category = document.getElementById("sCategory").value;
+    oPoint.latitude = document.getElementById("nLatitude").value;
+    oPoint.longitude = document.getElementById("nLongitude").value;;
     jsonPoint = JSON.stringify(oPoint);
     //console.log(jsonPoint);
-      $.ajax({
+    $.ajax({
         type: 'POST',
-        data: {point: jsonPoint},
+        data: { point: jsonPoint },
         datatype: 'json',
-        url: '/savePoint',
+        url: 'http://localhost:3001/savePoint',
+        success: function(data) {
+            console.log('saved document');
+        }
     });
 }
 
@@ -49,21 +54,21 @@ function getPointsOfInterest() {
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3001/getData',
-        success: function(data) {
+        success: function (data) {
             createTable(data);
         }
     });
 }
 
- function createTable(data) {
+function createTable(data) {
     var col = [];
-        for (var i = 0; i < data.length; i++) {
-            for (var key in data[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
+    for (var i = 0; i < data.length; i++) {
+        for (var key in data[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
             }
         }
+    }
 
     var table = document.createElement("table");
 
@@ -75,41 +80,44 @@ function getPointsOfInterest() {
         tr.appendChild(th);
     }
 
-     for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
 
-            tr = table.insertRow(-1);
+        tr = table.insertRow(-1);
 
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = data[i][col[j]];
-            }
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = data[i][col[j]];
         }
-        var divContainer = document.getElementById("showData");
-
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-    };
-
-    function saveDocument() {
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3001/saveDocument',
-            success: function(data) {
-                console.log('saved document');
-            }
-        });
     }
+    var divContainer = document.getElementById("showData");
 
-    function getDocument() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:3001/getDocument',
-            success: function(data) {
-                let divContainer = document.getElementById("showImage");
-                divContainer.innerHTML = ('<img src="' + data + '">');
-            }
-        });
-    }
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+};
+
+function saveDocument() {
+    let oImage = document.getElementById("oDocument").value;
+    let oImage2 = document.getElementById("oDocument").files[0].name;
+    $.ajax({
+        type: 'POST',
+        data: { object: oImage2 },
+        url: 'http://localhost:3001/saveDocument',
+        success: function (data) {
+            console.log('saved document');
+        }
+    });
+}
+
+function getDocument() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3001/getDocument',
+        success: function (data) {
+            let divContainer = document.getElementById("showImage");
+            divContainer.innerHTML = ('<img src="' + data + '">');
+        }
+    });
+}
 
 
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-let multer  =   require('multer');
+let multer = require('multer');
 let express = require('express');
 let http = require('http');
 let path = require('path');
@@ -12,7 +12,7 @@ let oSchema = Schema.sPointOfInterest();
 let jQuery = require('jQuery');
 let assert = require('assert');
 let dbMongo;
-const {ObjectID} = require("mongodb");
+const { ObjectID } = require("mongodb");
 let mongodb = require("mongodb");
 let Grid = require('gridfs-stream');
 Grid.mongo = mongoose.mongo;
@@ -20,10 +20,10 @@ let gfs;
 
 let schemaPoint = new mongoose.Schema({
     name: String, description: String, category: String, latitude: Number, longitude: Number
-     }); 
+});
 let Point = mongoose.model("Point", schemaPoint);
 let schemaRoute = new mongoose.Schema({
-    name: String, description:String, structure: [{ point: Object, order: Number}]
+    name: String, description: String, structure: [{ point: Object, order: Number }]
 });
 let Route = mongoose.model("Route", schemaRoute);
 
@@ -33,101 +33,103 @@ app.use(methodOverride());
 
 app.use('/', express.static(`${__dirname}/public`));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-let conn = mongoose.createConnection('mongodb://localhost:27017/maps');
-conn.once('open', function() {
+/**let conn = mongoose.createConnection('mongodb://localhost:27017/maps');
+conn.once('open', function () {
     console.log("connected");
     gfs = Grid(conn.db);
-})
+})**/
 
-app.post('/savePoint',function(req,res){
+mongoose.connect('mongodb://localhost:27017/maps');
+mongoose.connection.once('open', function() {
+    console.log("connected"); 
+});
+
+app.post('/savePoint', function (req, res) {
     let aResult = req.body.point;
     aResult = JSON.parse(aResult);
     let myData = new Point(aResult);
-   // myData =new Point({ name: "NameNew", description: "DescriptionNew", category: "CategoryNew", latitude: 4534, longitude: 2565});
-   
+    // myData =new Point({ name: "NameNew", description: "DescriptionNew", category: "CategoryNew", latitude: 4534, longitude: 2565});
     myData.save()
         .then(item => {
             res.send("item saved to database");
         })
-        .catch(err=> {
+        .catch(err => {
             res.status(400).send("unable to save to database");
         });
+});
 
-    });
-
-app.post('/saveRoute',function(req,res){
+app.post('/saveRoute', function (req, res) {
     //let aResult = req.body.route;
     //aResult = JSON.parse(aResult);
     //let oData = new Route(aResult);
-    let oData =new Route({ name: "Wein und Berg", description:"vorsicht bei Regen", structure: [{ point: '5bce180d2203630b1066887e', order: 1}, {point: {lat: 49.49669, long: 8.41267}, order : 2}, {point: '5bce18392203630b1066887f', order: 3}]});   
+    let oData = new Route({ name: "Wein und Berg", description: "vorsicht bei Regen", structure: [{ point: '5bce180d2203630b1066887e', order: 1 }, { point: { lat: 49.49669, long: 8.41267 }, order: 2 }, { point: '5bce18392203630b1066887f', order: 3 }] });
     oData.save()
         .then(item => {
             res.send("item saved to database");
         })
-        .catch(err=> {
+        .catch(err => {
             res.status(400).send("unable to save to database");
         });
 
-    });
+});
 
- app.get('/getData', function(req,res){
-    Point.find({}, function(err, data){
+app.get('/getData', function (req, res) {
+    Point.find({}, function (err, data) {
         res.send(data);
     });
- })   
+})
 
- app.get('/getLocalPoints', function(req,res) {
+app.get('/getLocalPoints', function (req, res) {
     let oBorder = req.query.border;
-    /** oBorder.dMinLat = parseFloat(oBorder.dMinLat);
-    oBorder.dMaxLat = parseFloat(oBorder.dMaxLat);
-    oBorder.dMinLong = parseFloat(oBorder.dMinLong);
-    oBorder.dMaxLong = parseFloat(oBorder.dMaxLong); **/
 
     let dMinLat = Number(oBorder.dMinLat);
     let dMaxLat = Number(oBorder.dMaxLat);
     let dMinLong = Number(oBorder.dMinLong);
     let dMaxLong = Number(oBorder.dMaxLong);
 
-    Point.find({latitude:{$gt:dMinLat, $lt:dMaxLat}}, function(err, data){
-        if(err) {throw err;}
+    Point.find({ latitude: { $gt: dMinLat, $lt: dMaxLat } }, function (err, data) {
+        if (err) { throw err; }
         res.send(data);
     });
-        
- })
 
-app.post('/saveDocument', function(req,res) {
-    let source = fs.createReadStream('test.jpg');
+})
+
+app.post('/saveDocument', function (req, res) {
+
+    let oObject = req.body.object;
+    oObject = 'test.jpg';
+    let source = fs.createReadStream(oObject);
     let target = gfs.createWriteStream({
         filename: 'test.jpg'
     });
     source.pipe(target);
 });
 
-app.get('/getDocument', function(req,res) {
+app.get('/getDocument', function (req, res) {
     //important: there must be only one file with this filename, otherwise no photo gets displayed
     let filename = 'test.jpg';
-    gfs.exist({ filename: filename }, (err,file) => {
+    gfs.exist({ filename: filename }, (err, file) => {
         if (err || !file) {
             res.status(404).send('File not Found');
             return
         }
         let data = [];
-        let readstream = gfs.createReadStream( {filename:filename});
-        readstream.on('data', function(result){
-           data.push(result);
+        let readstream = gfs.createReadStream({ filename: filename });
+        readstream.on('data', function (result) {
+            data.push(result);
         });
         readstream.on('end', function () {
             data = Buffer.concat(data);
             let img = 'data:image/jpg;base64,' + Buffer(data).toString('base64');
             res.send(img);
         });
-        readstream.on('err', function(err) {
+        readstream.on('err', function (err) {
             throw err;
         })
     })
@@ -163,9 +165,9 @@ let storage =   multer.diskStorage({
 });
 let upload = multer({ storage : storage}).single('userPhoto'); **/
 
-app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
-app.listen(3001,function(){
+app.listen(3001, function () {
     console.log("Working on port 3001");
 });
