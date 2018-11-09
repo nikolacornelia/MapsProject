1908d86b6326fcaef0d6ee124d6d22c46a8ad97d
PhilippKleemann <philipp.kleemann@yahoo.de>
2018-11-09 14:45:07 +0100

    User System anlegen

--- schema_model.js
+++ schema_model.js
@@ -5,16 +5,44 @@ var Schema = mongoose.Schema;
 module.exports = {
     sPointOfInterest: () => {
         var schema = new Schema({
-        pointOfInterest: { name: String, description: String, category: String, latitude: Number, longitude: Number}
+            pointOfInterest: { name: String, description: String, category: String, latitude: Number, longitude: Number }
         });
         let mSchema = mongoose.model('pointOfInterest', schema);
         return mSchema;
     },
     sRoute: () => {
         let schema = new Schema({
-        img: { data: Buffer, contentType: String }
+            img: { data: Buffer, contentType: String }
         });
-            let mSchema = mongoose.model('imageNew', schema);
-            return mSchema;
+        let mSchema = mongoose.model('imageNew', schema);
+        return mSchema;
+    },
+    sUser: () => {
+        var schema = new Schema({
+            user: {
+                email: {
+                    type: String,
+                    unique: true,
+                    required: true,
+                    trim: true
+                },
+                username: {
+                    type: String,
+                    unique: true,
+                    required: true,
+                    trim: true
+                },
+                password: {
+                    type: String,
+                    required: true,
+                },
+                passwordConf: {
+                    type: String,
+                    required: true,
+                }
+            }
+        });
+        let mSchema = mongoose.model(user, schema);
+        return mSchema;
     }
 } 
